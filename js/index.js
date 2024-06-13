e3ebe432e1bd1825404919455012a58a19c9d795
Mominul Islam <mominulislam462@gmail.com>
2024-06-14 01:17:31 +0600

    design added

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/* ....typing aniamtion script.......... */
+
+var typed = new Typed(".typing", {
+    strings: ["Data Scientist", "Full Stack Web Developer", "Researcher", "Machine Learning Developer", "Deep Learning Developer", "Natural Language Processing Developer", "Computer Vision Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+
+});
+
+
+var typed = new Typed(".typing-1", {
+    strings: ["Quantum ML","Quantum NLP","Data Science", "Full Stack Web Develop", "Machine Learning", "Deep Learning", "Natural Language Processing", "Computer Vision"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+
+
+});
